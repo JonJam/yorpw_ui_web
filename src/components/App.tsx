2baf5a1568f41a937031ec87349a083032a36e3c
Jonathan Harrison <jonjam1990@googlemail.com>
2017-08-12 19:42:15 +0100

    Added react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,34 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import HomePage from "./home/HomePage";
+import SitesPage from "./sites/SitesPage";
+import Error404Page from "./errors/Error404Page";
 
 class App extends React.Component {
   public render() {
-    return <HomePage />;
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sites">Sites</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/sites" component={SitesPage} />
+            <Route component={Error404Page} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
