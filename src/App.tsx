91fde6d33fdca5841da0c9a223c0eb3a6db7544d
Jonathan Harrison <jonjam1990@googlemail.com>
2017-08-12 18:46:15 +0100

    Added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 
-import "./App.css";
-
 class App extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>App</h1>
+          </div>
         </div>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
